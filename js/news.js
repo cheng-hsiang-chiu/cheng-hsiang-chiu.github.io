b6cfbdf7a76bb49be64805a2531d474c5b18a0dc
chchiu <chchiu@pass-2.ece.utah.edu>
2021-03-26 19:28:09 -0600

    update

--- js/news.js
+++ js/news.js
@@ -10,22 +10,22 @@ const news = [
     title: "Attendance @ ICCAD 2020",
     image_src: "images/2020_ICCAD-logo.jpg",
     image_url: "https://iccad.com/",
-    content: `I will attend the tutorial session talking about experience of using Taskflow to parallelize CAD<q><a href="https://iccad.com/event_details?id=305-12-D">GPU Acceleration in CAD: Opportunities and Challenges</a></q> at the 2020 IEEE/ACM International Conference on Computer-aided Design (ICCAD)!`,
-    footer: "Last updated on 2020/10/21"
+    content: `I attended the tutorial session talking about experience of using Taskflow to parallelize CAD<q><a href="https://iccad.com/event_details?id=305-12-D">GPU Acceleration in CAD: Opportunities and Challenges</a></q> at the 2020 IEEE/ACM International Conference on Computer-aided Design (ICCAD)!`,
+    footer: "Last updated on 2021/03/26"
   },
   {
     title: "Attendance @ WOSET 2020",
     image_src: "images/woset20.png",
     image_url: "https://woset-workshop.github.io/",
-    content: `I will attend the presentation of Taskflow at the 2020 Workshop of Open-source EDA Technology (WOSET) co-located with the International Conference on Computer-aided Design (ICCAD)!`,
-    footer: "Last updated on 2020/10/21"
+    content: `I attended the presentation of Taskflow at the 2020 Workshop of Open-source EDA Technology (WOSET) co-located with the International Conference on Computer-aided Design (ICCAD)!`,
+    footer: "Last updated on 2021/03/26"
   }, 
   {
     title: "Attendance @ MUC++ 2020",
     image_src: "images/mucpp-meetup.jpeg",
     image_url: "https://www.meetup.com/MUCplusplus/events/273223910/",
-    content: `I will attend the event presenting to the C++ group at München, Germany. <q><a href="https://www.meetup.com/MUCplusplus/events/273223910/">Taskflow: A General-purpose Parallel and Heterogeneous Task Programming System</a></q>, on Thu, Oct 22, 7:00 PM GMT+2.`,
-    footer: "Last updated on 2020/10/21"
+    content: `I attended the event presenting to the C++ group at München, Germany. <q><a href="https://www.meetup.com/MUCplusplus/events/273223910/">Taskflow: A General-purpose Parallel and Heterogeneous Task Programming System</a></q>, on Thu, Oct 22, 7:00 PM GMT+2.`,
+    footer: "Last updated on 2021/03/26"
   },
   {
     title: "Attendance @ CppCon 2020",
