764f56b1164b5a4c0e08934330e952eec3027470
chchiu <chenghsiang.chiu@gmail.com>
2022-04-05 22:22:07 -0600

    updated

--- js/news.js
+++ js/news.js
@@ -1,5 +1,13 @@
 const news = [
   {
+    title: "Paper accepted @ DAC 2022",
+    image_src: "images/2022_DAC_logo.png",
+    image_url: "https://www.dac.com",
+    content: `My paper "Efficient Timing Propagation with Simultaneous Structural and Pipeline Parallelisms" is accepted by DAC 2022.`,
+    footer: "Last updated on 2022/04/05"
+  },
+
+  {
     title: "Reviewer @ DAC 2022",
     image_src: "images/2022_DAC_logo.png",
     image_url: "https://www.dac.com",
