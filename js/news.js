b8571da662cb3fcb4ba7a3dc2aaf48f207894cb9
chchiu <chenghsiang.chiu@gmail.com>
2022-06-05 13:34:12 -0500

    update news

--- js/news.js
+++ js/news.js
@@ -1,5 +1,12 @@
 const news = [
   {
+  {
+    title: "Software Intern @ intel",
+    image_src: "images/intel.png",
+    image_url: "https://www.intel.com/content/www/us/en/developer/tools/oneapi/overview.html#gs.2apuov",
+    content: `My 2022 summer internship in intel is working on oneAPI and SYCL`,
+    footer: "Last updated on 2022/06/06"
+  },
     title: "Poster Accepted @ HPDC 2022",
     image_src: "images/2022_HPDC_logo.png",
     image_url: "https://www.hpdc.org/2022",
