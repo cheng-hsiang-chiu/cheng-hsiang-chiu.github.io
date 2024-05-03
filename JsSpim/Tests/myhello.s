04b42b9a7232945af53cc86628c578e6e9df1ada
cheng-hsiang-chiu <cchiu22@twhuang-desktop-10>
2024-05-03 13:15:00 -0500

    new

--- JsSpim/Tests/myhello.s
+++ JsSpim/Tests/myhello.s
@@ -1,3 +1,9 @@
+	.data
+	.align 2
+_a:	.space 4
+	.data
+	.align 2
+_b:	.space 4
 	.text
 	.globl main
 main:		# METHOD ENTRY
@@ -6,9 +12,10 @@ main:		# METHOD ENTRY
 	sw    $fp, 0($sp)	# PUSH
 	subu  $sp, $sp, 4
 	addu  $fp, $sp, 8
-		# WRITE
+	subu  $sp, $sp, 4
+			# WRITE
 	.data
-.L0:	.asciiz "Hello world!"
+.L0:	.asciiz "Hello world!\n"
 	.text
 	la    $t0, .L0
 	sw    $t0, 0($sp)	# PUSH
@@ -17,7 +24,16 @@ main:		# METHOD ENTRY
 	addu  $sp, $sp, 4
 	li    $v0, 4
 	syscall
-		# WRITE
+			# WRITE
+	.data
+	lw    $t0, 0($fp)
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	lw    $a0, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	li    $v0, 1
+	syscall
+			# WRITE
 	.data
 .L1:	.asciiz "\n"
 	.text
@@ -28,11 +44,143 @@ main:		# METHOD ENTRY
 	addu  $sp, $sp, 4
 	li    $v0, 4
 	syscall
-		# FUNCTION EXIT
+			# IF-THEN
+	.data
+	li    $t0, 2
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	li    $t0, 0
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	lw    $t1, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	lw    $t0, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	seq   $t0, $t0, $t1
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	lw    $t0, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	beq   $t0, 0, .L2
+			# WRITE
+	.data
+.L3:	.asciiz "Failed!\n"
+	.text
+	la    $t0, .L3
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	lw    $a0, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	li    $v0, 4
+	syscall
+.L2:
+			# IF-THEN
+	.data
+.L5:	.asciiz "ab"
+	.text
+	la    $t0, .L5
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+.L6:	.asciiz "ac"
+	.text
+	la    $t0, .L6
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	lw    $t1, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	lw    $t0, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	sne   $t0, $t0, $t1
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	lw    $t0, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	beq   $t0, 0, .L4
+			# WRITE
+	.data
+.L7:	.asciiz "Success!\n"
+	.text
+	la    $t0, .L7
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	lw    $a0, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	li    $v0, 4
+	syscall
+.L4:
+			# IF-THEN
+	.data
+	li    $t0, 1
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	li    $t0, 0
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	lw    $t1, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	lw    $t0, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	sgt   $t0, $t0, $t1
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	lw    $t0, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	beq   $t0, 0, .L8
+			# WRITE
+	.data
+.L9:	.asciiz "Failed!\n"
+	.text
+	la    $t0, .L9
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	lw    $a0, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	li    $v0, 4
+	syscall
+.L8:
+			# READ
+	.data
+	lw    $t0, 0($fp)
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	lw    $a0, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	li    $v0, 5
+	syscall
+			# WRITE
+	.data
+	lw    $t0, 0($fp)
+	sw    $t0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	lw    $a0, 4($sp)	# POP
+	addu  $sp, $sp, 4
+	li    $v0, 1
+	syscall
+			# FUNCTION EXIT
 _main_Exit:
-	lw    $ra, 0($fp)
-	move  $t0, $fp		# save control link
-	lw    $fp, -4($fp)	# restore FP
-	move  $sp, $t0		# restore SP
-	li $v0, 10     # load exit code for syscall
-	syscall        # only do this for main
+	lw    $ra, 0($fp)	# load return address
+	move  $t0, $fp
+	lw    $fp, -4($fp)
+	move  $sp, $t0
+	li    $v0, 10
+	syscall
+	.text
+myFunc:		# METHOD ENTRY
+	sw    $ra, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	sw    $fp, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	addu  $fp, $sp, 8
+	subu  $sp, $sp, 0
+	subu  $sp, $sp, 0
+	jal   main
+	sw    $v0, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+			# FUNCTION EXIT
+_myFunc_Exit:
+	lw    $ra, 0($fp)	# load return address
+	move  $t0, $fp
+	lw    $fp, -4($fp)
+	move  $sp, $t0
+	li    $v0, 10
+	syscall
